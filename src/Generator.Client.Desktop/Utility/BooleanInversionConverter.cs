0db3dc05813344404616f15152126172c51467c5
Andreas Müller <taori@users.noreply.github.com>
2018-12-31 12:16:45 +0100

    UI improvement

---
+++ src/Generator.Client.Desktop/Utility/BooleanInversionConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Generator.Client.Desktop.Utility
+{
+	public class BooleanInversionConverter : IValueConverter
+	{
+		/// <inheritdoc />
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool b)
+				return !b;
+
+			return value;
+		}
+
+		/// <inheritdoc />
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool b)
+				return !b;
+
+			return value;
+		}
+	}
+}
