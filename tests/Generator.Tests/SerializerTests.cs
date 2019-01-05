8ede754059e2fbed591207ec97212a01e4b05055
Andreas Müller <taori@users.noreply.github.com>
2019-01-05 02:47:48 +0100

    Tests

---
+++ tests/Generator.Tests/SerializerTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Generator.Shared.Serialization;
+using Xunit;
+
+namespace Generator.Tests
+{
+	public class SerializerTests
+	{
+		[Fact]
+		public void SerializePathIcon()
+		{
+//			var icon = new IconPackageReference()
+		}
+
+		[Fact]
+		public void SerializeVisualStudioIcon()
+		{
+
+		}
+	}
+}
